e8476bd0671afc0ff5ca0a1d0e2ce33a9592da0c
nguyenthanh1606 <nguyenthanh.bt1606@gmail.com>
2018-08-28 16:22:11 +0700

    Setup Data layer

--- OnlineShop.Data/Properties/AssemblyInfo.cs
+++ OnlineShop.Data/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0bbb9141-e13f-49c7-b6b1-1a70958222ff")]
+[assembly: Guid("dccb50e9-a9e3-49a1-813a-ca80fe6750a5")]
 
 // Version information for an assembly consists of the following four values:
 //
